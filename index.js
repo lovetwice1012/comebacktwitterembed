a6033ec71d9c81c20d97bcccc6d8f679154de087
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-10-10 21:00:15 +0900

    create

---
+++ index.js
@@ -0,0 +1,75 @@
+//discord.js v14
+const discord = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel] });
+const config = require('./config.json');
+const fetch = require('node-fetch');
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ready!`);
+});
+
+client.on('messageCreate', async (message) => {
+    console.log(message.content)
+    if (message.author.bot) return;
+    if ((message.content.includes('twitter.com') || message.content.includes('x.com')) && message.content.includes('status')) {
+        const url = message.content.match(/(https?:\/\/[^\s]+)/g);
+        url.forEach(element => {
+            //replace twitter.com or x.com with api.vxtwitter.com
+            var newUrl = element.replace(/twitter.com|x.com/g, 'api.vxtwitter.com');
+            if (newUrl.split("/").length > 6) {
+                newUrl = newUrl.split("/").slice(0, 6).join("/");
+            }
+            //fetch the api
+            fetch(newUrl)
+                .then(res => res.json())
+                .then(json => {
+                    attachments = [];
+                    const embed = {
+                        title: json.user_name,
+                        url: json.tweetURL,
+                        description: json.text,
+                        color: 0x1DA1F2,
+                        author: {
+                            name: json.user_screen_name,
+                        }
+                    };
+                    //if the tweet has media
+                    if (json.mediaURLs) {
+                        if (json.mediaURLs.length == 1 && json.mediaURLs[0].includes('pbs.twimg.com')) {
+                            embed.image = {
+                                url: json.mediaURLs[0]
+                            }
+                        } else {
+                            attachments = json.mediaURLs
+                        }
+                    }
+                    if (attachments.length > 0) {
+                        message.reply({
+                            embeds: [
+                                embed
+                            ],
+                            files: attachments,
+                            allowedMentions: {
+                                repliedUser: false
+                            }
+                        })
+                    } else {
+                        message.reply({
+                            embeds: [
+                                embed
+                            ],
+                            allowedMentions: {
+                                repliedUser: false
+                            }
+                        })
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        });
+    }
+});
+
+client.login(config.token);
