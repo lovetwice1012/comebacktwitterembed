1dbdcaee91e237c838323ad1d5ed3d1f90a5712f
yuyutti <yuyutti@yuyutti.com>
2023-11-12 00:09:18 +0900

    localize

---
+++ test/create.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+const locales = require('./lang.js'); // Import the locales from lang.js
+
+// Function to convert a locale to .resx format
+function convertToResx(localeObj, lang) {
+    const builder = new xml2js.Builder({ headless: true });
+    const root = {
+        root: {
+            data: Object.keys(localeObj).map(key => ({
+                '$': { name: key },
+                value: [localeObj[key][lang]]
+            }))
+        }
+    };
+
+    return builder.buildObject(root);
+}
+
+// Create 'locales' directory if it doesn't exist
+const dir = './locales';
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+// Convert and save for each language
+['en', 'ja'].forEach(lang => {
+    const resxContent = convertToResx(locales, lang);
+    const fileName = `${dir}/${lang === 'en' ? 'en-US.resx' : lang + '.resx'}`;
+    fs.writeFileSync(fileName, resxContent);
+    console.log(`Saved ${fileName}`);
+});
---
+++ test/lang.js
@@ -0,0 +1,467 @@
+module.exports = {
+    LocalizedTranslate : {
+        ja: '翻訳',
+        en: 'translate'
+    },
+
+    LocalizedDelete : {
+        ja: '削除',
+        en: 'delete'
+    },
+
+    localizedDefaultLanguage : {
+        ja: '言語',
+        en: 'Language'
+    },
+
+    LocalizedAll : {
+        ja: 'すべて',
+        en: 'all'
+    },
+
+    LocalizedHelp : {
+        ja: 'ヘルプ',
+        en: 'help'
+    },
+
+    LocalizedPing : {
+        ja: '遅延確認',
+        en: 'ping'
+    },
+
+    LocalizedInvite : {
+        ja: '招待',
+        en: 'invite'
+    },
+
+    LocalizedSupport : {
+        ja: 'サポート',
+        en: 'support'
+    },
+
+    LocalizedSettings : {
+        ja: '設定',
+        en: 'settings'
+    },
+
+    LocalizedDisable : {
+        ja: '無効化',
+        en: 'disable'
+    },
+
+    LocalizedBannedwords : {
+        ja: '禁止ワード',
+        en: 'bannedwords'
+    },
+
+    LocalizedUser : {
+        ja: 'ユーザー',
+        en: 'user'
+    },
+
+    LocalizedChannel : {
+        ja: 'チャンネル',
+        en: 'channel'
+    },
+
+    LocalizedRole : {
+        ja: 'ロール',
+        en: 'role'
+    },
+
+    LocalizedWord : {
+        ja: '単語',
+        en: 'word'
+    },
+
+    LocalizedDefaultlanguage : {
+        ja: 'デフォルト言語',
+        en: 'defaultlanguage'
+    },
+
+    LocalizedPing : {
+        ja: 'Pong!',
+        en: 'Pong!'
+    },
+
+    LocalizedFinishaction : {
+        ja: '操作を完了しました。',
+        en: 'Finished action.'
+    },
+
+    LocalizedShowattachmentsasembedsimagebutton : {
+        ja: '画像を埋め込み画像として表示する',
+        en: 'Show media in embeds image'
+    },
+
+    LocalizedShowmediaasattachmentsbutton : {
+        ja: 'メディアを添付ファイルとして表示する',
+        en: 'Show media as attachments'
+    },
+
+    LocalizedHelpcommanddiscription : {
+        ja: 'ヘルプメッセージを表示します。',
+        en: 'Shows help message.'
+    },
+
+    LocalizedHelpdiscription : {
+        ja: '特別な設定は必要なく、ツイートリンクを投稿するだけで使用することができます。\n\nこのbotは、あなたが送信したメッセージの内容を確認することができます。\nあなたが送信したメッセージにtwitterのリンクが含まれているかどうかを確認するためだけに使用され、それ以外の目的で使用されることはありません。\nまた、あなたが送信したメッセージを記録することもありません。\n私たちを信頼できない場合は、このbotからチャンネルの閲覧権限を削除することで安全を確保することができます。',
+        en: 'No special setup is required, just post the tweet link.\n\nThis bot can check the contents of messages you have sent.\nIt will only be used to check if the message you sent contains a twitter link, and will not be used for any other purpose.\nIt will not be used for any other purpose, nor will it record the messages you send.\nIf you do not trust us, you can secure your safety by removing your channel permissions from this bot.'
+    },
+
+    LocalizedHelpcommands : {
+        ja: '`/ping` - Pong!\n`/help` - ヘルプメッセージを表示します。\n`/invite` - このbotをあなたのサーバーに招待するためのリンクを表示します\n`/support` - サポートサーバーに参加するためのリンクを表示します\n`/settings` - 設定を変更します',
+        en: '`/ping` - Pong!\n`/help` - Shows help message.\n`/invite` - Invite me to your server!\n`/support` - Join support server!\n`/settings` - chenge Settings'
+    },
+
+    LocalizedSettingsdisablediscription : {
+        ja: 'ユーザーまたはチャンネルを指定して無効化します。',
+        en: 'Disable by user or channel.'
+    },
+
+    LocalizedSettingsbannedwordsdiscription : {
+        ja: '禁止ワードを追加または削除します。',
+        en: 'Add or remove banned words.'
+    },
+
+    LocalizedSettingsdisableuserdiscription : {
+        ja: '無効化するユーザーを指定します。',
+        en: 'Specify the user to disable.'
+    },
+
+    LocalizedSettingsdisablechanneldiscription : {
+        ja: '無効化するチャンネルを指定します。',
+        en: 'Specify the channel to disable.'
+    },
+
+    LocalizedSettingsbannedwordsworddiscription : {
+        ja: '禁止ワードを指定します。',
+        en: 'Specify the banned word.'
+    },
+
+    LocalizedUserdonthavepermission : {
+        ja: 'このコマンドを使用する権限がありません。',
+        en: 'You don\'t have permission to use this command.'
+    },
+
+    LocalizedUsercantusethiscommandforotherusers : {
+        ja: 'ユーザーにあなた以外のユーザーを指定することはできません。',
+        en: 'You can\'t use this command for other users.'
+    },
+
+    LocalizedUsercantdeletethismessage : {
+        ja: 'このメッセージを削除することはできません。',
+        en: 'You can\'t delete this message.'
+    },
+
+    LocalizedUsermustspecifyauserorchannel : {
+        ja: 'ユーザーまたはチャンネル、ロールのうち一つを指定する必要があります。',
+        en: 'You must specify a user or channel or role.'
+    },
+
+    LocalizedUsercantspecifybothauserandachannel : {
+        ja: '複数のオプションを指定することはできません。',
+        en: 'You can\'t specify multiple options.'
+    },
+
+    LocalizedIdonthavepermissiontomanagemessages : {
+        ja: 'BOTにメッセージを管理する権限がありません。',
+        en: 'I don\'t have permission to manage messages.'
+    },
+
+    LocalizedIdonthavepermissiontodeletemessages : {
+        ja: 'BOTにメッセージを削除する権限がありません。',
+        en: 'I don\'t have permission to delete messages.'
+    },
+
+    LocalizedAddedusertodisableuser : {
+        ja: '無効化するユーザーに追加しました。',
+        en: 'Added user to disable.user.'
+    },
+
+    LocalizedRemoveduserfromdisableuser : {
+        ja: '無効化するユーザーから削除しました。',
+        en: 'Removed user from disable.user.'
+    },
+
+    LocalizedAddedchanneltodisablechannel : {
+        ja: '無効化するチャンネルに追加しました。',
+        en: 'Added channel to disable.channel.'
+    },
+
+    LocalizedRemovedchannelfromdisablechannel : {
+        ja: '無効化するチャンネルから削除しました。',
+        en: 'Removed channel from disable.channel.'
+    },
+
+    LocalizedAddedroletodisablerole : {
+        ja: '無効化するロールに追加しました。',
+        en: 'Added role to disable.role.'
+    },
+
+    LocalizedRemovedrolefromdisablerole : {
+        ja: '無効化するロールから削除しました。',
+        en: 'Removed role from disable.role.'
+    },
+
+    LocalizedAddedwordtobannedwords : {
+        ja: '禁止ワードに追加しました。',
+        en: 'Added word to bannedWords.'
+    },
+
+    LocalizedRemovedwordfrombannedwords : {
+        ja: '禁止ワードから削除しました。',
+        en: 'Removed word from bannedWords.'
+    },
+
+    LocalizedUsermustspecifyanyword : {
+        ja: 'オプションを正確に指定する必要があります。',
+        en: 'You must specify a option.'
+    },
+
+    LocalizedDefaultlanguagediscription : {
+        ja: '翻訳するときのデフォルトの言語を設定します。',
+        en: 'Sets the default language when translating.'
+    },
+
+    LocalizedEditoriginaliftranslatediscription : {
+        ja: '翻訳するときにオリジナルのメッセージを編集するかどうかを設定します。',
+        en: 'Sets whether to edit the original message when translating.'
+    },
+
+    LocalizedbotInvitelink : {
+        ja: 'このbotをあなたのサーバーに招待するためのリンクを表示します',
+        en: 'Invite me to your server!'
+    },
+
+    LocalizedSupportInvitelink : {
+        ja: 'サポートサーバーに参加するためのリンクを表示します',
+        en: 'Join support server!'
+    },
+
+    LocalizedSettingschange : {
+        ja: '設定を変更します',
+        en: 'chenge Settings'
+    },
+
+    LocalizedYourcontentsisconteinbannedword : {
+        ja: 'あなたのメッセージには禁止ワードが含まれています。',
+        en: 'Your message contains a banned word.'
+    },
+
+    LocalizedIdonthavedeletemessagepermission : {
+        ja: 'メッセージを削除する権限がありません。',
+        en: 'I don\'t have permission to delete messages.',
+    },
+
+    LocalizedSetdefaultlanguageto : {
+        ja: 'デフォルトの言語を設定しました。 :',
+        en: 'Set default language to '
+    },
+
+    LocalizedSeteditoriginaliftranslateto : {
+        ja: 'editOriginalIfTranslateを設定しました。 :',
+        en: 'Set editOriginalIfTranslate to '
+    },
+
+    LocalizedYoucantdeleteotherusersmessages : {
+        ja: 'あなたは他のユーザーのメッセージを削除することはできません。',
+        en: 'You can\'t delete other users\' messages.'
+    },
+
+    LocalizedSettingssendmediaasattachmentsasdefaultdescriptionlocalizations : {
+        ja: 'メディアを添付ファイルとして表示するかどうかを設定します。',
+        en: 'Sets whether to show media as attachments.'
+    },
+
+    LocalizedSettingsdeletemessageifonlypostedtweetlinkdescriptionlocalizations : {
+        ja: 'ツイートのリンクのみを投稿した場合にメッセージを削除するかどうかを設定します。',
+        en: 'Sets whether to delete the message if only the tweet link is posted.'
+    },
+
+    LocalizedSettingsalwaysreplyifpostedtweetlinkdescriptionlocalizations : {
+        ja: 'ツイートのリンクを投稿した場合に常に返信するかどうかを設定します。',
+        en: 'Sets whether to always reply if the tweet link is posted.'
+    },
+
+    LocalizedSetdefaultmediaasattachmentsto : {
+        ja: 'メディアを添付ファイルとして表示するかどうかを設定しました。 :',
+        en: 'Set sendMediaAsAttachmentsAsDefault to '
+    },
+
+    LocalizedSetdeleteifonlypostedtweetlinkto : {
+        ja: 'ツイートのリンクのみを投稿した場合にメッセージを削除するかどうかを設定しました。 :',
+        en: 'Set deleteIfOnlyPostedTweetLink to '
+    },
+
+    LocalizedSetalwaysreplyifpostedtweetlinkto : {
+        ja: 'ツイートのリンクを投稿した場合に常に返信するかどうかを設定しました。 :',
+        en: 'Set alwaysReplyIfPostedTweetLink to '
+    },
+
+    LocalizedAddedallbutton : {
+        ja: 'すべてのボタンを無効化しました。',
+        en: 'Disabled all buttons.'
+    },
+
+    LocalizedRemovedallbutton : {
+        ja: 'すべてのボタンを有効化しました。',
+        en: 'Enabled all buttons.'
+    },
+
+    LocalizedSetshowmediaasattachmentsbutton : {
+        ja: 'メディアを添付ファイルとして表示するボタンを設定しました。 :',
+        en: 'Set showMediaAsAttachments button to '
+    },
+
+    LocalizedSetshowattachmentsasembedsimagebutton : {
+        ja: '画像を埋め込み画像として表示するボタンを設定しました。 :',
+        en: 'Set showAttachmentsAsEmbedsImage button to '
+    },
+
+    LocalizedSettranslatebutton : {
+        ja: '翻訳ボタンを設定しました。 :',
+        en: 'Set translate button to '
+    },
+
+    LocalizedSetdeletebutton : {
+        ja: '削除ボタンを設定しました。 :',
+        en: 'Set delete button to '
+    },
+
+    LocalizedAddedshowmediaasattachmentsbutton : {
+        ja: 'メディアを添付ファイルとして表示するボタンを無効化しました。',
+        en: 'Disabled showMediaAsAttachments button.'
+    },
+
+    LocalizedRemovedshowmediaasattachmentsbutton : {
+        ja: 'メディアを添付ファイルとして表示するボタンを有効化しました。',
+        en: 'Enabled showMediaAsAttachments button.'
+    },
+
+    LocalizedAddedshowattachmentsasembedsimagebutton : {
+        ja: '画像を埋め込み画像として表示するボタンを無効化しました。',
+        en: 'Disabled showAttachmentsAsEmbedsImage button.'
+    },
+
+    LocalizedRemovedshowattachmentsasembedsimagebutton : {
+        ja: '画像を埋め込み画像として表示するボタンを有効化しました。',
+        en: 'Enabled showAttachmentsAsEmbedsImage button.'
+    },
+
+    LocalizedAddedtranslatebutton : {
+        ja: '翻訳ボタンを無効化しました。',
+        en: 'Disabled translate button.'
+    },
+
+    LocalizedRemovedtranslatebutton : {
+        ja: '翻訳ボタンを有効化しました。',
+        en: 'Enabled translate button.'
+    },
+
+    LocalizedAddeddeletebutton : {
+        ja: '削除ボタンを無効化しました。',
+        en: 'Disabled delete button.'
+    },
+
+    LocalizedRemoveddeletebutton : {
+        ja: '削除ボタンを有効化しました。',
+        en: 'Enabled delete button.'
+    },
+
+    LocalizedAddedusertobuttondisableduser : {
+        ja: 'ボタンを無効化するユーザーに追加しました。',
+        en: 'Added user to button_disabled.user.'
+    },
+
+    LocalizedRemoveduserfrombuttondisableduser : {
+        ja: 'ボタンを無効化するユーザーから削除しました。',
+        en: 'Removed user from button_disabled.user.'
+    },
+
+    LocalizedAddedchanneltobuttondisabledchannel : {
+        ja: 'ボタンを無効化するチャンネルに追加しました。',
+        en: 'Added channel to button_disabled.channel.'
+    },
+
+    LocalizedRemovedchannelfrombuttondisabledchannel : {
+        ja: 'ボタンを無効化するチャンネルから削除しました。',
+        en: 'Removed channel from button_disabled.channel.'
+    },
+
+    LocalizedAddedroletobuttondisabledrole : {
+        ja: 'ボタンを無効化するロールに追加しました。',
+        en: 'Added role to button_disabled.role.'
+    },
+
+    LocalizedRemovedrolefrombuttondisabledrole : {
+
+        ja: 'ボタンを無効化するロールから削除しました。',
+        en: 'Removed role from button_disabled.role.'
+    },
+
+    LocalizedSettingsextractbotmessagedescriptionlocalizations : {
+        ja: 'BOTのメッセージを展開するかどうかを設定します。',
+        en: 'Sets whether to extract bot messages.'
+    },
+
+    LocalizedSetextractbotmessageto : {
+        ja: 'BOTのメッセージを展開するかどうかを設定しました。 :',
+        en: 'Set extractBotMessage to '
+    },
+
+    LocalizedEditoriginaliftranslate : {
+        ja: '翻訳時にオリジナルのメッセージを編集',
+        en: 'editoriginaliftranslate'
+    },
+
+    LocalizedBoolean : {
+        ja: 'はいかいいえ',
+        en: 'boolean'
+    },
+
+    LocalizedSetdefaultmediaasattachments : {
+        ja: 'メディアを添付ファイルとして表示',
+        en: 'setdefaultmediaasattachments'
+    },
+
+    LocalizedDeleteifonlypostedtweetlink : {
+        ja: 'ツイートのリンクのみを投稿した場合にメッセージを削除',
+        en: 'deleteifonlypostedtweetlink'
+    },
+
+    LocalizedAlwaysreplyifpostedtweetlink : {
+        ja: 'ツイートのリンクを投稿した場合に常に返信',
+        en: 'alwaysreplyifpostedtweetlink'
+    },
+
+    LocalizedButton : {
+        ja: 'ボタン',
+        en: 'button'
+    },
+
+    LocalizedInvisible : {
+        ja: '非表示',
+        en: 'invisible'
+    },
+
+    LocalizedDisabled : {
+        ja: '無効化',
+        en: 'disabled'
+    },
+
+    LocalizedExtractbotmessage : {
+        ja: 'ボットのメッセージを展開',
+        en: 'extractbotmessage'
+    },
+
+    LocalizedShowmediaasattachments : {
+        ja: 'メディアを添付ファイルとして表示',
+        en: 'showmediaasattachments'
+    },
+
+    LocalizedShowattachmentsasembedsimage : {
+        ja: '画像を埋め込み画像として表示',
+        en: 'showattachmentsasembedsimage'
+    },
+};
