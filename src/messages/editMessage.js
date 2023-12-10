3cc6297411b0aa430dcdd1313f9271bf2b17bf3e
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-12-10 12:35:23 +0900

    Add message editing functionality to editMessage and postMessage classes

--- src/messages/editMessage.js
+++ src/messages/editMessage.js
@@ -1,5 +1,12 @@
+const message = require('./message');
 class editMessage extends message {
-
+    constructor(messageClass, plan, result = null, error = null) {
+        super(messageClass, plan, result, error);
+    }
+    
+    send(message){
+        this.message.edit(message);
+    }
 }
 
 module.exports = editMessage;
--- src/messages/postMessage.js
+++ src/messages/postMessage.js
@@ -1,5 +1,12 @@
+const message = require('./message');
 class postMessage extends message {
+    constructor(messageClass, plan, result = null, error = null) {
+        super(messageClass, plan, result, error);
+    }
 
+    send(message){
+        this.message.channel.send(message);
+    }
 }
 
 module.exports = postMessage;
