193decad9d0e16377b3bc97b78ce1981380d40da
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-12-10 12:37:11 +0900

    Add message class constructor and send method

--- src/messages/message.js
+++ src/messages/message.js
@@ -1,5 +1,21 @@
 class message {
-
+    message = null;
+    plan = null;
+    result = null;
+    error = null;
+    time = null;
+    
+    constructor(messageClass, plan, result = null, error = null) {
+        this.message = messageClass;
+        this.plan = plan;
+        this.result = result;
+        this.error = error;
+        this.time = new Date();
+    }
+    
+    send(message){
+        this.message.channel.send(message);
+    }
 }
 
 module.exports = message;
