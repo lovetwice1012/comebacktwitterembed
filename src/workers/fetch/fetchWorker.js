492f4a713c5b145908e7d225fb19a4c0a33df64b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2023-12-31 14:07:23 +0900

    Add console.log statement for debugging

--- src/workers/fetch/fetchWorker.js
+++ src/workers/fetch/fetchWorker.js
@@ -31,6 +31,7 @@ parentPort.on("message", (data) => {
         }, 100);
         return;
     }
+    console.log(data)
     let result = null;
     let url = data.url.replace(/twitter.com/g, "fxapi.lovetwice1012.workers.dev").replace(/x.com/g, "fxapi.lovetwice1012.workers.dev");
     //テスト用
