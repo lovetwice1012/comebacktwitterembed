2a3bdbd567e5a8dcd105c18c885e0a94853cc3dc
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-03-02 10:53:45 +0900

    新着自動展開機能の登録解除メッセージ修正

--- index.js
+++ index.js
@@ -1019,7 +1019,7 @@ hint	text NULL
                 client.users.fetch(result.userid).then(user => {
                     user.send({embeds: [{
                         title: '新着自動展開機能の登録が自動解除されました',
-                        description: `あなたが登録した新着自動展開機能の登録(登録ID:${result.rssId}は、以下の理由により自動解除されました。\n\n理由: ${result.reason}\n\n詳細: \n${result.hint}`,
+                        description: `あなたが登録した新着自動展開機能の登録(登録ID:${result.rssId})は、以下の理由により自動解除されました。\n\n理由: ${result.reason}\n\n詳細: \n${result.hint}`,
                         color: 0x1DA1F2
                     }]}).then(() => {
                         connection.query('UPDATE deregister_notification SET sendedDirectMessage = 1 WHERE \'index\' = ?', [result.index], (err, results, fields) => {
