b3ed1e41a531e996d3f5746cf79e12163e07926e
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-10-05 11:03:18 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ const fs = require('fs');
 
 function antiDirectoryTraversalAttack(userInput) {
     const baseDirectory = path.resolve('saves');
-    if (userInput.includes('\0')) {
-        throw new Error('不正なパスが検出されました。');
-    }
     const invalidPathPattern = /(\.\.(\/|\\|$))/;
-    if (invalidPathPattern.test(userInput)) {
-        throw new Error('不正なパスが検出されました。');
-    }
     const joinedPath = path.join(baseDirectory, userInput);
     let realPath;
     try {
@@ -28,6 +22,8 @@ function antiDirectoryTraversalAttack(userInput) {
     }
     const relativePath = path.relative(baseDirectory, realPath);
     if (
+        userInput.includes('\0') ||
+        invalidPathPattern.test(userInput) ||
         relativePath.startsWith('..') ||
         path.isAbsolute(relativePath) ||
         relativePath.includes('\0') ||
